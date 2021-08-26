9d2d09f9faa743fd0b54194a396260958cd5c1a6
Lucas Urquiza <urquizaxd@gmail.com>
2021-08-26 09:44:03 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {WalletFlatList} from './src/screens/WalletFlatList';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => WalletFlatList);
